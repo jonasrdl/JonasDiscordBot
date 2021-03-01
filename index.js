1a52e68645afa5baa573c46d95328184dc89af9e
Jonas Riedel <jonasriedel@gmx.net>
2021-03-01 22:06:03 +0100

    added cc status and servers command

--- index.js
+++ index.js
@@ -9,6 +9,31 @@ client.on('ready', () => {
   command(client, ['ping', 'test'], (message) => {
     message.channel.send('Pong')
   })
+
+  command(client, 'servers', (message) => {
+    client.guilds.cache.forEach((guild) => {
+      message.channel.send(`${guild.name} has ${guild.memberCount} members`)
+    })
+  })
+
+  command(client, ['cc', 'clearchannel'], (message) => {
+    if (message.member.hasPermission('ADMINISTRATOR')) {
+      message.channel.messages.fetch().then((results) => {
+        message.channel.bulkDelete(results)
+      })
+    }
+  })
+
+  command(client, 'status', (message) => {
+    const content = message.content.replace('.status ', '')
+
+    client.user.setPresence({
+      activity: {
+        name: content,
+        type: 0,
+      },
+    })
+  })
 })
 
 client.login(config.token)
