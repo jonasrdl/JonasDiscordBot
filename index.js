30eb94c3964ae8cb4113ee777a3a85fd0660e252
Jonas Riedel <jonasriedel@gmx.net>
2021-03-08 21:39:01 +0100

    ADD: .offend command, .help command (embed)

--- index.js
+++ index.js
@@ -10,14 +10,6 @@ client.on('ready', () => {
 
 client.on('message', (message) => {
     if (!message.member.user.bot && message.guild) {
-        if (message.content === '.help') {
-            message.channel.send('Hallo Welt');
-        }
-    }
-});
-
-client.on('message', (message) => {
-    if (!message.member.user.bot && message.guild) {
         if (message.content.startsWith('.avatar')) {
             if (message.mentions.users.first()) {
                 let user = message.mentions.users.first();
@@ -29,6 +21,31 @@ client.on('message', (message) => {
             }
             console.log(message.member.user.tag + ' executed command .avatar');
         }
+    }
+});
+
+client.on('message', (message) => {
+    if (!message.member.user.bot && message.guild) {
+        if (message.mentions.users.first()) {
+            let user = message.mentions.users.first();
+            message.channel.send(`${user}` + ' is stupid :^)');
+        }
+    }
+});
+
+client.on('message', (message) => {
+    if (message.content === '.help' && message.guild && !message.member.user.bot) {
+        let helpEmbed = new Discord.MessageEmbed()
+            .setColor('#1f5e87')
+            .setTitle('Help Syntax')
+            .setURL('https://github.com/jonasrdl')
+            .setAuthor('Jonas', 'https://cdn.discordapp.com/avatars/209945797918195712/00b1d6f82c7c2525a18a1a8cb44a0ce6.png?size=128')
+            .setDescription('Help')
+            .addField('.offend @[Username]', 'Offend someone', false)
+            .addField('.avatar @[Username]', 'Get a users avatar', false)
+            .setTimestamp();
+
+        message.channel.send(helpEmbed);
     }
 });
 
