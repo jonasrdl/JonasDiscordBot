545eec1ae5f8deadb032fbdaa3dae6f12962d5e5
Jonas Riedel <jonasriedel@gmx.net>
2021-04-07 17:05:18 +0200

    added quote command

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const Discord = require('discord.js');
 const fs = require('fs');
 const config = JSON.parse(fs.readFileSync('config.json', 'utf-8'));
 const client = new Discord.Client();
+const quotes = [
+  'Any time you try to create an Internet meme, automatic fail. Thats like the worst thing you can do.',
+  'I feel in todays time, you are truly successful if a meme is based on you.',
+  'Memes just show that people are engaged about something. A meme is just a little inside joke for a group of people that care about a certain thing.',
+  'I have a great sense of humor, I mean even when my meme came out I reposted it. I didnt care. I was laughing, too!',
+  'I dont even know what memes are, Im, like, an old person, so I dont really know what a meme is.',
+  'I am so happy to be a meme.',
+  'Some people only recognize me for that - aint you the meme guy?',
+  'I run a meme type of account on Twitter; I know what my audience is looking for.',
+];
 
 let time;
 let date;
@@ -17,9 +27,6 @@ function getDate() {
 }
 
 setInterval(getTime, 1000);
-
-//const getTime = () => new Date().toLocaleTimeString('de', { hour12: false });
-//const getDate = () => new Date().toLocaleDateString();
 
 client.on('ready', () => {
   console.log('Bot logged in as ' + client.user.tag);
@@ -88,6 +95,19 @@ client.on('message', (message) => {
       .then(() => console.log('Sent private Message'));
   }
 
+  // Quote Command
+  if (message.content === '.quote') {
+    const randomQuote = Math.floor(Math.random() * quotes.length);
+
+    let quoteEmbed = new Discord.MessageEmbed()
+      .setColor('#1f5e87')
+      .setTitle(quotes[randomQuote]);
+
+    message.channel
+      .send(quoteEmbed)
+      .then(() => console.log('Executed .quote command'));
+  }
+
   // Help Embed command
   if (
     message.content === '.help' &&
@@ -108,6 +128,7 @@ client.on('message', (message) => {
       .addField('.time', 'Get the current Time', false)
       .addField('.date', 'Get the current Date', false)
       .addField('.dm', 'Send yourself a private message', false)
+      .addField('.quote', 'Get a random Quote', false)
       .setTimestamp();
 
     message.channel.send(helpEmbed).then(() => console.log('helpEmbed sent'));
