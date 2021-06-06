61b6ba7f628fc4011dbc9678be3b460fce306477
Jonas Riedel <jonasriedel@gmx.net>
2021-06-06 18:41:29 +0200

    Fixed uptime

--- commands/info.js
+++ commands/info.js
@@ -2,28 +2,15 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 let prefix = '.';
 
-function uptime() {
-  let days = Math.floor(client.uptime / 86400000);
-  let hours = Math.floor(client.uptime / 3600000) % 24;
-  let minutes = Math.floor(client.uptime / 60000) % 60;
-  let seconds = Math.floor(client.uptime / 1000) % 60;
-
-  return (
-    days +
-    ' Days, ' +
-    hours +
-    ' Hours, ' +
-    minutes +
-    ' Minutes, ' +
-    seconds +
-    ' Seconds'
-  );
-}
-
 module.exports = {
   name: 'info',
   description: 'General info about the Bot',
   execute(client, message, args, Discord) {
+    let days = Math.floor(client.uptime / 86400000);
+    let hours = Math.floor(client.uptime / 3600000) % 24;
+    let minutes = Math.floor(client.uptime / 60000) % 60;
+    let seconds = Math.floor(client.uptime / 1000) % 60;
+
     let infoEmbed = new Discord.MessageEmbed()
       .setColor('#1f5e87')
       .setTitle('Information')
@@ -31,7 +18,18 @@ module.exports = {
       .addField('Serious project?', 'No, obv its fun.', false)
       .addField('Need help?', prefix + 'help', false)
       .addField('Who made me?', prefix + 'author', false)
-      .addField('Uptime', uptime(), false)
+      .addField(
+        'Uptime',
+        days +
+          ' Days, ' +
+          hours +
+          ' Hours, ' +
+          minutes +
+          ' Minutes, ' +
+          seconds +
+          ' Seconds',
+        false
+      )
       .setTimestamp();
 
     message.channel.send(infoEmbed);
