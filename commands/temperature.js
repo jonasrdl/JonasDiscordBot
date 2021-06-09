4444696e2c286d5fd2501785a0dc14b335d863e7
Jonas Riedel <jonasriedel@gmx.net>
2021-06-09 18:54:46 +0200

    Added try catch and formatted code for better visibility

--- commands/temperature.js
+++ commands/temperature.js
@@ -2,36 +2,27 @@ const Discord = require('discord.js');
 const fetch = require('node-fetch');
 const fs = require('fs');
 const config = JSON.parse(fs.readFileSync('./config/config.json', 'utf-8'));
-const api_token = JSON.parse(fs.readFileSync('./config/config.json', 'utf-8'));
 let prefix = '.';
 
 module.exports = {
-  name: 'temperature',
-  description: 'Get the temperature of any city',
-  execute(client, message) {
-    prefix = '.';
-
+  name: 'temperature', description: 'Get the temperature of any city', execute(client, message) {
+    const api_token = JSON.parse(fs.readFileSync('./config/config.json', 'utf-8'));
     const args = message.content.slice(prefix.length).trim().split(' ');
     const command = args.shift().toLowerCase();
 
+    prefix = '.';
+
     if (!args.length) {
-      message.channel.send(
-        'No City specified.\n Need help? ->' + prefix + 'help'
-      );
+      message.channel.send('No City specified.\n Need help? ->' + prefix + 'help');
       return;
     }
 
-    fetch(
-      'https://api.openweathermap.org/data/2.5/weather?q=' +
-      args +
-      '&units=metric&appid=' +
-      config.api_token
-    )
-      .then((response) => response.json())
-      .then((data) =>
-        message.channel.send(
-          'The temperature in ' + args + ' is: ' + data.main.temp + '°C'
-        )
-      );
-  },
+    try {
+      fetch('https://api.openweathermap.org/data/2.5/weather?q=' + args + '&units=metric&appid=' + config.api_token)
+          .then((response) => response.json())
+          .then((data) => message.channel.send('The temperature in ' + args + ' is: ' + data.main.temp + '°C'));
+    } catch (error) {
+      console.error(error);
+    }
+  }
 };
