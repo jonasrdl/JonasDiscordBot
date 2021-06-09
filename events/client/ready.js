6df63ed2389d9f245f9918e33868511bf71f6e3a
Jonas Riedel <jonasriedel@gmx.net>
2021-06-09 16:36:45 +0200

    Logging to the console if activity was set

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,5 +1,5 @@
 module.exports = (Discord, client) => {
   console.log('Bot is online!');
 
-  client.user.setActivity('.help', { type: 'PLAYING' });
+  client.user.setActivity('.help', {type: 'PLAYING'}).then(activity => console.log('Activity set!'));
 };
