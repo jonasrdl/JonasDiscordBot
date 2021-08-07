95dd682dea9515d93743d9e0108d0ea7e1e01eb9
jonasrdl <jonasriedel@gmx.net>
2021-08-07 14:16:39 +0200

    Removed js files from git (now ts)

--- src/commands/js/author.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'author',
-  description: 'Infos about the author',
-  execute(client, message, args, Discord) {
-    let authorEmbed = new Discord.MessageEmbed().setColor('#1f5e87').setTitle('Reach me on: ').addField('Github', 'https://github.com/jonasrdl', false).addField('Website', 'https://jonasriedel.com', false).addField('Twitter', 'https://twitter.com/jvnxs7', false).setTimestamp();
-
-    message.channel.send(authorEmbed).then(() => console.log('Author embed sent'));
-  },
-};
