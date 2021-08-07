314db172f1cd65713a4fb21c37cab32119738ef0
jonasrdl <jonasriedel@gmx.net>
2021-08-07 16:37:37 +0200

    Minified

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,11 +1,7 @@
-function sendPong(): string {
-  return 'pong';
-}
-
 module.exports = {
   name: 'ping',
   description: 'Ping pong',
   execute(client, message, args, Discord) {
-    message.channel.send(sendPong);
+    message.channel.send('Test');
   },
 };
