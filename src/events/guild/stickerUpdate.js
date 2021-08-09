32a16aa7f690f6b71672ad4b00cbe058660c47e6
jriedel <jriedel@united-internet.de>
2021-08-09 14:12:08 +0200

    added sticker update event

---
+++ src/events/guild/stickerUpdate.js
@@ -0,0 +1,5 @@
+module.exports = (Discord, client) =>  {
+  console.log("Sticker update");
+
+  client.channels.get('874262998728982588').send('There was just a sticker update! Check it out.');  
+}
