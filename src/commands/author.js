2be7cc5c41a7051fe152e9fe17c5722d7222208f
Jonas <jonasriedel@gmx.net>
2021-10-10 16:11:06 +0200

    Website no longer under maintenance

--- src/commands/author.js
+++ src/commands/author.js
@@ -10,7 +10,7 @@ module.exports = {
             .setColor('#1f5e87')
             .setTitle('Reach me on: ')
             .addField('Github', 'https://github.com/jonasrdl', false)
-            .addField('Website (under maintenance)', 'https://jonasriedel.com', false)
+            .addField('Website', 'https://jonasriedel.com', false)
             .addField('Twitter', 'https://twitter.com/jvnxs7', false)
             .addField('Keybase', 'https://keybase.io/jonasrdl', false)
             .setTimestamp();
