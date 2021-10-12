33c9b0b0f5108e988c03cf43c19fa9a011d186d5
Jonas <jonasriedel@gmx.net>
2021-10-12 13:50:13 +0200

    Add express webserver for weather report

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-const { Client, Collection, Intents } = require('discord.js')
+const { Client, Collection, Intents, MessageEmbed } = require('discord.js')
 const fs = require('fs')
-const { token } = require('./config.json')
+const { token, apiToken } = require('./config.json')
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const PORT = 55689
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+
+const app = express()
+app.use(cookieParser())
+
+app.get(`/sendWeatherMessage`, (req, res) => {
+    const URL = 'https://wttr.in/Karlsruhe.png?m'
+    const channelID = '897428889607999509'
+    const channel = client.channels.cache.get(channelID)
+    let cookieFromClient = req.cookies['key']
+
+    if (cookieFromClient === apiToken) {
+        const embed = new MessageEmbed()
+            .setColor('#1f5e87')
+            .setTitle('Daily weather report for Karlsruhe')
+            .setImage(URL)
+            .setTimestamp()
+
+        channel.send({ embeds: [embed] })   
+    } else {
+        res.send('Unauthorized')
+    }
+})
 
 client.commands = new Collection()
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
@@ -33,6 +58,10 @@ client.on('interactionCreate', async interaction => {
         console.error(error)
         return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
     }
+})
+
+app.listen(PORT, () => {
+    console.log('=> Express server running')
 })
 
 client.login(token)
