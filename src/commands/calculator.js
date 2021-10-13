97357317de76e270ad1f423a83dfb3b353771031
Jonas R <jonasriedel@gmx.net>
2021-10-13 13:40:00 +0200

    Fixed calculator

--- src/commands/calculator.js
+++ src/commands/calculator.js
@@ -1,37 +1,69 @@
 const { MessageEmbed } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders')
 
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('calculator')
-        .setDescription('calculate two numbers')
-        .addStringOption(option => option
-            .setName('firstnumber')
-            .setDescription('First number'))
-        .addStringOption(option => option
-            .setName('operator')
-            .setDescription('Operator'))
-      .addStringOption(option => option
-            .setName('secondnumber')
-            .setDescription('Second number')),
-    async execute(interaction) {
-        const firstNumber = interaction.options.getString('firstnumber')
-        const operator = interaction.options.getString('operator')
-        const secondNumber = interaction.options.getString('secondnumber')
+const validateNumbers = (number) => {
+  return !isNaN(number)
+}
 
-        if (operator === '+') {
-            return interaction.reply(Number(firstNumber) + Number(secondNumber))
-          } else if (operator === '-') {
-            return interaction.reply(Number(firstNumber) - Number(secondNumber))
-          } else if (operator === '/') {
-            if (secondNumber === 0) {
-              message.channel.send('Division by 0 is not allowed!' + ' 🧠🤓');
-              return;
-            }
-      
-            return interaction.reply(Number(firstNumber) / Number(secondNumber))
-          } else if (operator === '*') {
-            return interaction.reply(Number(firstNumber) * Number(secondNumber))
-          }
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('calculator')
+    .setDescription('calculate two numbers')
+    .addStringOption((option) => option.setName('calculation').setDescription('Calculation you wanna make')),
+  async execute(interaction) {
+    const calculation = interaction.options.getString('calculation')
+    const calculationArr = calculation.split(' ')
+    const firstNumber = calculationArr[0]
+    const operator = calculationArr[1]
+    const secondNumber = calculationArr[2]
+
+    let result
+
+    if (firstNumber.includes('(') || firstNumber.includes(')')) {
+      const embed = new MessageEmbed()
+        .setColor('#b60707')
+        .setTitle('Special characters are not yet implemented.')
+        .setTimestamp()
+
+      return interaction.reply({ embeds: [embed] })
     }
+
+    switch (operator) {
+      case '+':
+        result = Number(firstNumber) + Number(secondNumber)
+        break
+      case '-':
+        result = Number(firstNumber) - Number(secondNumber)
+        break
+      case '/':
+        if (Number(secondNumber) === 0) {
+          const embed = new MessageEmbed()
+            .setColor('#b60707')
+            .setTitle('Division by 0 is invalid.')
+            .setTimestamp()
+
+          return interaction.reply({ embeds: [embed] })
+        }
+
+        result = Number(firstNumber) / Number(secondNumber)
+        break
+      case '*':
+        result = Number(firstNumber) * Number(secondNumber)
+        break
+      default:
+        const embed = new MessageEmbed()
+          .setColor('#b60707')
+          .setTitle('Invalid operator, try again!')
+          .setTimestamp()
+
+        return interaction.reply({ embeds: [embed] })
+    }
+
+    const embed = new MessageEmbed()
+      .setColor('#1f5e87')
+      .setTitle(String(result))
+      .setTimestamp()
+
+    return interaction.reply({ embeds: [embed] })
+  }
 }
