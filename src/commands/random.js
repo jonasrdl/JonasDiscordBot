791349ece3c54d69f00976b6191024d4cdc606d8
Jonas R <jonasriedel@gmx.net>
2021-10-13 13:56:45 +0200

    Implement random command

--- src/commands/random.js
+++ src/commands/random.js
@@ -1,11 +1,35 @@
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require('discord.js')
 const { SlashCommandBuilder } = require('@discordjs/builders')
+const fetch = require('node-fetch');
+
+const removeQuotes = str => {
+    return str.replace(/^"(.+(?="$))"$/, '$1')
+}
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('random')
         .setDescription('Get random user from API'),
     async execute(interaction) {
-        return interaction.reply()
+        fetch(`https://api.randomuser.me/`)
+          .then(response => response.json())
+          .then(data => {
+              const firstName = removeQuotes(JSON.stringify(data.results[0].name.first))
+              const lastName = removeQuotes(JSON.stringify(data.results[0].name.last))
+              const age = removeQuotes(JSON.stringify(data.results[0].dob.age))
+              const picture = data.results[0].picture.large
+
+              const embed = new MessageEmbed()
+                .setColor('#1f5e87')
+                .setURL('https://api.randomuser.me/')
+                .setTitle('Random User: ')
+                .addField('First name: ', firstName, false)
+                .addField('Last name: ', lastName, false)
+                .addField('Age: ', age, false)
+                .setImage(picture)
+                .setTimestamp()
+
+              return interaction.reply({ embeds: [embed] })
+          })
     }
 }
