e0c6f6c3c5bc25310bdc0845ba82875603bf5dde
Jonas R <jonasriedel@gmx.net>
2021-10-13 11:37:57 +0200

    Add check for city

--- src/commands/weather.js
+++ src/commands/weather.js
@@ -1,6 +1,10 @@
 const { MessageEmbed } = require('discord.js')
 const { SlashCommandBuilder } = require('@discordjs/builders')
 
+const isCityValid = (city) => {
+    return !isNaN(city);
+}
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("weather")
@@ -8,13 +12,15 @@ module.exports = {
         .addStringOption((option) => option.setName("city").setDescription("City")),
     async execute(interaction) {
         const city = interaction.options.getString("city")
-        
-        const embed = new MessageEmbed()
-            .setColor('#1f5e87')
-            .setTitle(`${city}`)
-            .setImage(`https://wttr.in/${city}.png`)
-            .setTimestamp()
+
+        if (isCityValid()) {
+            const embed = new MessageEmbed()
+              .setColor('#1f5e87')
+              .setTitle(`${city}`)
+              .setImage(`https://wttr.in/${city}.png`)
+              .setTimestamp()
 
             return interaction.reply({ embeds: [embed] })
         }
     }
+}
