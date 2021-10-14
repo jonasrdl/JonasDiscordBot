d442bc0b17d2373ad60fea8154cb69a070be36ea
Jonas R <jonasriedel@gmx.net>
2021-10-14 09:58:05 +0200

    Fix url parameters and city check

--- src/commands/weather.js
+++ src/commands/weather.js
@@ -1,10 +1,6 @@
 const { MessageEmbed } = require('discord.js')
 const { SlashCommandBuilder } = require('@discordjs/builders')
 
-const isCityNumber = (interaction, city) => {
-        return !isNaN(city)
-}
-
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("weather")
@@ -13,14 +9,14 @@ module.exports = {
     async execute(interaction) {
         const city = interaction.options.getString("city")
 
-        if (!isCityNumber()) {
+        if (!isNaN(city)) {
             return interaction.reply('City is invalid, try again!')
         }
 
         const embed = new MessageEmbed()
           .setColor('#1f5e87')
           .setTitle(`${city}`)
-          .setImage(`https://wttr.in/${city}.png`)
+          .setImage(`https://wttr.in/${city}.png?m`)
           .setTimestamp()
 
         return interaction.reply({ embeds: [embed] })
