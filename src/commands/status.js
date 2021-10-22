f3f850f46a6140568a14972972dae2962ecc83e3
Jonas <jonasriedel@protonmail.com>
2021-10-22 23:51:42 +0200

    Add status cmd

---
+++ src/commands/status.js
@@ -0,0 +1,33 @@
+const { MessageEmbed } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { guildId } = require('../config.json')
+const fetch = require('node-fetch')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('status')
+        .setDescription('Check if services are online'),
+    async execute(interaction, client) {
+        fetch('http://172.17.0.1/api/website')
+          .then(data => data.json())
+          .then(data => {
+            console.log(data)
+          })
+
+        let websiteOnline
+
+        /* const embed = new MessageEmbed()
+            .setColor('#1f5e87')
+            .setTitle('Server information')
+            .addField('Name', `${guild.name}`, false)
+            .addField('Members', `${guild.memberCount}`)
+            .addField('Created at', `${guild.createdAt}`, false)
+            .setImage(guild.iconURL())
+            .setTimestamp();
+
+        */    
+  
+        //return interaction.reply({ embeds: [embed] })
+        return interaction.reply('Test')
+    }
+}
