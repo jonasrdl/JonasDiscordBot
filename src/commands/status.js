74d870b5e585af8037609ed619c8d91d3de5129f
Jonas <jonasriedel@protonmail.com>
2021-10-22 23:59:05 +0200

    Fix status command, working now

--- src/commands/status.js
+++ src/commands/status.js
@@ -9,22 +9,24 @@ module.exports = {
         .setDescription('Check if services are online'),
     async execute(interaction, client) {
         fetch('http://status.jonasriedel.com/api/website')
-          .then(data => console.log(data))
-          
-        let websiteOnline
+          .then(data => {
+            if (data.status === 200) {
+              const successEmbed = new MessageEmbed()
+                .setColor('#1f5e87')
+                .setTitle('Status')
+                .addField('Website', 'Online ✔️', false)
+                .setTimestamp();
 
-        /* const embed = new MessageEmbed()
-            .setColor('#1f5e87')
-            .setTitle('Server information')
-            .addField('Name', `${guild.name}`, false)
-            .addField('Members', `${guild.memberCount}`)
-            .addField('Created at', `${guild.createdAt}`, false)
-            .setImage(guild.iconURL())
-            .setTimestamp();
+                interaction.reply({ embeds: [successEmbed] })
+            } else {
+              const failEmbed = new MessageEmbed()
+                .setColor('#1f5e87')
+                .setTitle('Status')
+                .addField('Website', 'Offline ❌', false)
+                .setTimestamp();
 
-        */    
-  
-        //return interaction.reply({ embeds: [embed] })
-        return interaction.reply('Test')
+                interaction.reply({ embeds: [failEmbed] })
+            }
+          })
     }
 }
