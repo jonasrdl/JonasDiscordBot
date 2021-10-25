e3976e07f55d0eb63ef03fbb9e6c8cc0327ed5e0
Jonas <jonasriedel@protonmail.com>
2021-10-25 20:18:04 +0200

    Removed weekend check, formatted

--- src/index.js
+++ src/index.js
@@ -13,133 +13,120 @@ const app = express()
 app.use(cors())
 app.use(cookieParser())
 
-const isWeekend = () => {
-    const today = new Date()
-
-    if (today.getDay() == 6 || today.getDay() == 0) {
-        return true
-    }    
-}
-
 app.get(`/isOnline`, (req, res) => {
-    res.sendStatus(200)
+  res.sendStatus(200)
 })
 
 app.get(`/sendWeatherMessage`, (req, res) => {
-    const channelID = '897428889607999509'
-    const channel = client.channels.cache.get(channelID)
-    let cookieFromClient = req.cookies['key']
+  const channelID = '897428889607999509'
+  const channel = client.channels.cache.get(channelID)
+  let cookieFromClient = req.cookies['key']
 
-    if (cookieFromClient === apiToken) {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=Karlsruhe&appid=${weatherApiToken}&units=metric`)
-            .then(data => data.json())
-            .then(data => {
-                const temperature = data.main.temp
-                const temperatureFeelsLike = data.main.feels_like
-                const guild = client.guilds.cache.get(guildId)
+  if (cookieFromClient === apiToken) {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=Karlsruhe&appid=${weatherApiToken}&units=metric`
+    )
+      .then((data) => data.json())
+      .then((data) => {
+        const temperature = data.main.temp
+        const temperatureFeelsLike = data.main.feels_like
+        const guild = client.guilds.cache.get(guildId)
 
-                if (temperature <= 6) {
-                    if (isWeekend) {
-                        return
-                    }
+        if (temperature <= 6) {
+          guild.members.fetch(userID).then((user) => {
+            user.send('The temperature is under 6°C, its cold!')
+          })
+        }
 
-                    guild.members.fetch(userID)
-                        .then(user => {
-                            user.send('The temperature is under 6°C, its cold!')
-                        })
-                }
+        if (data.weather[0].main === 'Rain') {
+          guild.members.fetch(userID).then((user) => {
+            user.send('Watch out, its raining!')
+          })
+        }
 
-                if (data.weather[0].main === 'Rain') {
-                    if (isWeekend) {
-                        return
-                    }
+        const embed = new MessageEmbed()
+          .setColor('#1f5e87')
+          .setTitle(`Daily weather for Karlsruhe`)
+          .addField('Temperature', `${temperature}°C`, false)
+          .addField('Feels like', `${temperatureFeelsLike}°C`, false)
+          .addField('Weather', `${data.weather[0].main}`)
+          .setTimestamp()
 
-                    guild.members.fetch(userID)
-                        .then(user => {
-                            user.send('Watch out, its raining!')
-                        })
-                }
+        channel.send({ embeds: [embed] })
+      })
 
-                const embed = new MessageEmbed()
-                    .setColor('#1f5e87')
-                    .setTitle(`Daily weather for Karlsruhe`)
-                    .addField('Temperature', `${temperature}°C`, false)
-                    .addField('Feels like', `${temperatureFeelsLike}°C`, false)
-                    .addField('Weather', `${data.weather[0].main}`)
-                    .setTimestamp()
+    res.send('Successful')
+  } else {
+    res.status(401).send('Unauthorized')
 
-                channel.send({ embeds: [embed] })  
-            })
-
-        res.send('Successful')
-    } else {
-        res.status(401).send('Unauthorized')
-
-        channel.send('Weather could not be sent. There was a problem.')
-    }
+    channel.send('Weather could not be sent. There was a problem.')
+  }
 })
 
 app.get('/sendNasaPOTD', (req, res) => {
-    const URL = `https://api.nasa.gov/planetary/apod?api_key=${nasaToken}`
-    const channelID = '898644879784181790'
-    const channel = client.channels.cache.get(channelID)
-    let cookieFromClient = req.cookies['key']
+  const URL = `https://api.nasa.gov/planetary/apod?api_key=${nasaToken}`
+  const channelID = '898644879784181790'
+  const channel = client.channels.cache.get(channelID)
+  let cookieFromClient = req.cookies['key']
 
-    if (cookieFromClient === apiToken) {
-        fetch (`https://api.nasa.gov/planetary/apod?api_key=${nasaToken}`)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data)
+  if (cookieFromClient === apiToken) {
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${nasaToken}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
 
-                const embed = new MessageEmbed()
-                .setColor('#1f5e87')
-                .setTitle('' + data.title)
-                .setDescription('' + data.explanation)
-                .setImage(data.hdurl)
-                .setTimestamp()
+        const embed = new MessageEmbed()
+          .setColor('#1f5e87')
+          .setTitle('' + data.title)
+          .setDescription('' + data.explanation)
+          .setImage(data.hdurl)
+          .setTimestamp()
 
-            channel.send({ embeds: [embed] })
-      })  
+        channel.send({ embeds: [embed] })
+      })
 
-        res.send('Successful')
-    } else {
-        res.status(401).send('Unauthorized')
-    }
+    res.send('Successful')
+  } else {
+    res.status(401).send('Unauthorized')
+  }
 })
 
 client.commands = new Collection()
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'))
 
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`)
-    client.commands.set(command.data.name, command)
+  const command = require(`./commands/${file}`)
+  client.commands.set(command.data.name, command)
 }
 
 client.once('ready', () => {
-    console.log('Bot started!')
+  console.log('Bot started!')
 })
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) {
-        return
-    }
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) {
+    return
+  }
 
-    const command = client.commands.get(interaction.commandName)
+  const command = client.commands.get(interaction.commandName)
 
-    if (!command) {
-        return
-    }
+  if (!command) {
+    return
+  }
 
-    try {
-        await command.execute(interaction, client)
-    } catch (error) {
-        console.error(error)
-        return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
-    }
+  try {
+    await command.execute(interaction, client)
+  } catch (error) {
+    console.error(error)
+    return interaction.reply({
+      content: 'There was an error while executing this command!',
+      ephemeral: true,
+    })
+  }
 })
 
 app.listen(PORT, () => {
-    console.log('=> Express server running')
+  console.log('=> Express server running')
 })
 
 client.login(token).then(() => console.log('Bot logged in!'))
