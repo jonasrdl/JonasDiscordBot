7f1068d0536215d389ac62757e3be1f60f7f212c
Jonas <jonasriedel@protonmail.com>
2021-11-25 18:20:24 +0100

    Add corona command (incidence)

---
+++ src/commands/corona.js
@@ -0,0 +1,21 @@
+const { MessageEmbed } = require('discord.js')
+const { SlashCommandBuilder } = require('@discordjs/builders')
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('corona')
+    .setDescription('Get infos about the current corona situation.'),
+  async execute(interaction) {
+    fetch(`http://172.17.0.1:55690/incidence/stadtkreis/`)
+      .then((response) => response.json())
+      .then((data) => {
+        const embed = new MessageEmbed()
+          .setColor('#1f5e87')
+          .setTitle('Karlsruhe Inzidenz')
+          .addField('Stadtkreis', data.incidence)
+          .setTimestamp()
+
+        return interaction.reply({ embeds: [embed] })
+      })
+  }
+}
