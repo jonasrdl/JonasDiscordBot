640e0b03faf8f51a9083080e84d24467b9b88558
Jonas <jonasriedel@protonmail.com>
2022-01-02 02:48:23 +0100

    Fix typo

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const { REST } = require('@discordjs/rest')
 const { Routes } = require('discord-api-types/v9')
-require('dotent').config()
+require('dotenv').config()
 
 const commands = []
 const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'))
