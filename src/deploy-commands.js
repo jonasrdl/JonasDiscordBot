86f6d9d49dd45cbff5e87a95407a5add123d27e7
Jonas Riedel <jonasriedel@protonmail.com>
2022-01-03 11:51:29 +0100

    Sync dev (#43)
    * Dev (#34)
    
    * Fix private key info
    
    * Fix CI
    
    * Fix CI
    
    * Fix uses
    
    Co-authored-by: Jonas <jriedel@united-internet.de>
    
    * Add commands to CI (#35)
    
    * Fix private key info
    
    * Fix CI
    
    * Fix CI
    
    * Fix uses
    
    * Add commands to CI
    
    Co-authored-by: Jonas <jriedel@united-internet.de>
    
    * Implement custom location and some fixes (#42)
    
    * Port config changes to env
    
    * Remove import
    
    * Location stuff
    
    * String literals
    
    * String literals
    
    * Maybe fix env
    
    * Add error handling maybe fix request
    
    * Remove response.json
    
    * Stuff
    
    * stuff
    
    * stuff
    
    * Fix
    
    * Test
    
    * Remove debug message
    
    * Update activity text
    
    * Update heading
    
    * Switch to .env
    
    * Fix typo
    
    * Fix server command
    
    Co-authored-by: Jonas <jriedel@united-internet.de>

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,19 +1,22 @@
 const fs = require('fs')
 const { REST } = require('@discordjs/rest')
 const { Routes } = require('discord-api-types/v9')
-
-const { clientId, guildId, token } = require('./config.json')
+require('dotenv').config()
 
 const commands = []
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'))
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`)
-	commands.push(command.data.toJSON())
+  const command = require(`./commands/${file}`)
+  commands.push(command.data.toJSON())
 }
 
-const rest = new REST({ version: '9' }).setToken(token)
+const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_BOT_TOKEN)
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error)
+rest
+  .put(
+    Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID, process.env.DISCORD_GUILD_ID),
+    { body: commands }
+  )
+  .then(() => console.log('Successfully registered application commands.'))
+  .catch(console.error)
