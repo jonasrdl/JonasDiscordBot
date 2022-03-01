a8581fce61d311d306c4a051c61f9910531472a6
jonasrdl <jonasriedel@protonmail.com>
2022-03-01 20:51:56 +0100

    Add host infos and move methods to util class

--- src/commands/info/stats.ts
+++ src/commands/info/stats.ts
@@ -1,26 +1,11 @@
 import { Command } from '../../structures/Command';
 import { MessageEmbed, version } from 'discord.js';
 import os from 'os';
-
-const determineOs = (os: string): string => {
-    console.log(os);
-
-    if (os.includes('arch')) {
-        return 'Arch Linux';
-    } else if (os.includes('ubuntu')) {
-        return 'Ubuntu';
-    } else if (os.includes('Windows')) {
-        return 'Windows';
-    } else if (os.includes('mac')) {
-        return 'MacOS';
-    } else {
-        return 'Unknown OS!';
-    }
-};
+import Util from '../../util/Util';
 
 export default new Command({
     name: 'stats',
-    description: 'Stats about the bot',
+    description: 'Stats about the host',
     run: async ({ client, interaction }) => {
         const days: number = Math.floor(client.uptime / 86400000);
         const hours: number = Math.floor(client.uptime / 3600000) % 24;
@@ -29,51 +14,31 @@ export default new Command({
 
         const embed: MessageEmbed = new MessageEmbed()
             .setColor('#1f5e87')
-            .setTitle('Stats')
+            .setTitle('Stats about host')
             .addField(
                 'RAM',
-                `${memory(os.totalmem() - os.freemem(), false)} / ${memory(
-                    os.totalmem()
-                )}`,
+                `${Util.memory(
+                    os.totalmem() - os.freemem(),
+                    false
+                )} / ${Util.memory(os.totalmem())}`,
                 false
             )
             .addField('Discord.js', `v${version}`, false)
             .addField('Node.js', `${process.version}`, false)
             .addField('Operating system', `${process.platform}`)
             .addField('Kernel', `${os.release()}`)
-            .addField('CPU', `${os.cpus()[0].model}`)
+            .addField('Architecture', `${os.arch()}`)
+            .addField('Hostname', `${os.hostname()}`)
             .addField(
-                'Bot Uptime',
-                days +
-                    ' Days, ' +
-                    hours +
-                    ' Hours, ' +
-                    minutes +
-                    ' Minutes, ' +
-                    seconds +
-                    ' Seconds',
-                false
-            );
+                'Average load time',
+                `1min: ${os.loadavg()[0]}\n 5min: ${os.loadavg()[1]}\n 15min: ${
+                    os.loadavg()[2]
+                }
+            `
+            )
+            .addField('CPU', `${os.cpus()[0].model}`)
+            .addField('Uptime', `${Util.convertTime(os.uptime)}`);
 
         return interaction.followUp({ embeds: [embed] });
     },
 });
-
-function memory(bytes = 0, r = true) {
-    const gigaBytes = bytes / 1024 ** 3;
-    if (gigaBytes > 1) {
-        return `${gigaBytes.toFixed(1)} ${r ? 'GB' : ''}`;
-    }
-
-    const megaBytes = bytes / 1024 ** 2;
-    if (megaBytes > 1) {
-        return `${megaBytes.toFixed(2)} ${r ? 'MB' : ''}`;
-    }
-
-    const kiloBytes = bytes / 1024;
-    if (kiloBytes > 1) {
-        return `${kiloBytes.toFixed(2)} ${r ? 'KB' : ''}`;
-    }
-
-    return `${bytes.toFixed(2)} ${r ? 'B' : ''}`;
-}
---
+++ src/util/Util.ts
@@ -0,0 +1,38 @@
+class Util {
+    static convertTime = (seconds: any) => {
+        let convert = (x: any) => {
+            return x < 10 ? '0' + x : x;
+        };
+
+        return (
+            convert(Math.floor(seconds / (60 * 60))) +
+            'h ' +
+            convert(Math.floor(seconds / 60) % 60) +
+            'm ' +
+            convert(Math.floor(seconds % 60)) +
+            's'
+        );
+    };
+
+    static memory = (bytes = 0, r = true) => {
+        const gigaBytes = bytes / 1024 ** 3;
+
+        if (gigaBytes > 1) {
+            return `${gigaBytes.toFixed(1)} ${r ? 'GB' : ''}`;
+        }
+
+        const megaBytes = bytes / 1024 ** 2;
+        if (megaBytes > 1) {
+            return `${megaBytes.toFixed(2)} ${r ? 'MB' : ''}`;
+        }
+
+        const kiloBytes = bytes / 1024;
+        if (kiloBytes > 1) {
+            return `${kiloBytes.toFixed(2)} ${r ? 'KB' : ''}`;
+        }
+
+        return `${bytes.toFixed(2)} ${r ? 'B' : ''}`;
+    };
+}
+
+export default Util;
