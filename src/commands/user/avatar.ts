3e63d665c492c34cd71ce915ead923f833157879
jonasrdl <jonasriedel@protonmail.com>
2022-03-11 10:07:36 +0100

    Remove debug log

--- src/commands/user/avatar.ts
+++ src/commands/user/avatar.ts
@@ -1,30 +1,28 @@
 // @ts-nocheck
 
-import { Command } from '../../structures/Command'
-import { MessageEmbed } from 'discord.js'
+import { Command } from '../../structures/Command';
+import { MessageEmbed } from 'discord.js';
 
 export default new Command({
-  name: 'avatar',
-  description: 'get avatar of a user',
-  options: [
-    {
-      name: 'target',
-      description: 'Person to get their avatar',
-      type: 'MENTIONABLE',
-      required: true,
-    },
-  ],
-  run: async ({ interaction }) => {
-    const user = interaction.options.getMentionable('target')
+    name: 'avatar',
+    description: 'get avatar of a user',
+    options: [
+        {
+            name: 'target',
+            description: 'Person to get their avatar',
+            type: 'MENTIONABLE',
+            required: true
+        }
+    ],
+    run: async ({ interaction }) => {
+        const user = interaction.options.getMentionable('target');
 
-    console.log(user)
+        const embed = new MessageEmbed()
+            .setColor('#1f5e87')
+            .setTitle('Avatar')
+            .setImage(user.user.displayAvatarURL({ dynamic: true, size: 512 }))
+            .setTimestamp();
 
-    const embed = new MessageEmbed()
-      .setColor('#1f5e87')
-      .setTitle('Avatar')
-      .setImage(user.user.displayAvatarURL({ dynamic: true, size: 512 }))
-      .setTimestamp()
-
-    return interaction.followUp({ embeds: [embed] })
-  },
-})
+        return interaction.followUp({ embeds: [embed] });
+    }
+});
