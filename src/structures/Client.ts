65bedb64513369f204b1ad924c734f40eb8361d3
jonas <jonasriedel@protonmail.com>
2022-09-13 15:35:41 +0200

    change to global

--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -37,7 +37,7 @@ export class ExtendedClient extends Client {
 
   async registerCommands({ commands }: RegisterCommandsOptions): Promise<void> {
     const guildId: string = process.env.guildId;
-    let global: boolean = false;
+    let global: boolean = true;
 
     if (global) {
       // Global
